17d6c90a4b91bb5b15f8f356ba15e4eae2e1e8a3
Isaac Obuya <Szack4477@gmail.com>
2020-10-01 13:02:10 +0300

    added cors

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require("express");
+const cors = require('cors');
 const bodyParser = require("body-parser");
 
 require("./src/db/db");
@@ -11,6 +12,7 @@ const EndTripRegister = require("./src/routes/EndTripRegister");
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
