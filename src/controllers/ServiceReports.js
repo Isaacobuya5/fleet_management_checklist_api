e8fedfad1f38423b3316e936a74ea6db5ca33597
Isaac Obuya <Szack4477@gmail.com>
2020-10-02 09:02:48 +0300

    extract selected garage

--- src/controllers/ServiceReports.js
+++ src/controllers/ServiceReports.js
@@ -1,9 +1,17 @@
 const ServiceReport = require("../model/ServicingReport");
 
 const serviceReport = async (req, res) => {
-    
+    const { vehicle_registration, current_odometer_reading, service_due_date, garage, parts_needed, comments, date, time, lat, long } = req.body;
+    const [ selectedGarage ] = garage;
     try {
-        const service = new ServiceReport(req.body);
+        const service = new ServiceReport({
+            vehicle_registration,
+            current_odometer_reading, 
+            service_due_date, 
+            garage: selectedGarage, 
+            parts_needed, 
+            comments
+        });
         await service.save();
         res.status(201).send(service);
     } catch (error) {
